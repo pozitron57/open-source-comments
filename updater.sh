b20b4ffac3dbde3511922dd5c35ab8ca95f21b60
pozitron57 <lisakov57@gmail.com>
2018-07-16 13:01:00 +0300

    automatic update

--- updater.sh
+++ updater.sh
@@ -12,3 +12,5 @@ rsync -auvx --delete --numeric-ids data.js index.html /var/www/lisakov.com/proje
 rsync -auvx --delete --numeric-ids images/ /var/www/lisakov.com/projects/open-source-comments/images/
 rsync -auvx --delete --numeric-ids css/ /var/www/lisakov.com/projects/open-source-comments/css/
 rsync -auvx --delete --numeric-ids js/ /var/www/lisakov.com/projects/open-source-comments/js/
+
+# update github repo  https://github.com/pozitron57/open-source-comments
