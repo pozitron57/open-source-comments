6f6541f269ea17764e729cdd7960404a259ed4eb
pozitron57 <lisakov57@gmail.com>
2018-07-16 13:03:28 +0300

    automatic update

--- updater.sh
+++ updater.sh
@@ -14,6 +14,7 @@ rsync -auvx --delete --numeric-ids css/ /var/www/lisakov.com/projects/open-sourc
 rsync -auvx --delete --numeric-ids js/ /var/www/lisakov.com/projects/open-source-comments/js/
 
 # update github repo  https://github.com/pozitron57/open-source-comments
-git push origin master
+git pull
 git add *
 git commit -m 'automatic update'
+git push origin master
