3fbcd41478f75f5bbfcc2f8222c6db4c0d48879c
pozitron57 <lisakov57@gmail.com>
2018-08-21 13:31:22 +0300

    add echo for logging

--- updater.sh
+++ updater.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 cd /home/slisakov/open-source-comments
+echo 'git pull'
 nice -n5 git pull
 
 # Update the data on gh stars, last commit etc
+echo 'python3 get_gh_data.py'
 nice -n5 python3 get_gh_data.py
 
 # generate data.js (read by index.html)
+echo 'python3 yaml_2_js.py'
 nice -n5 python3 yaml_2_js.py
 
 # deploy changes
+echo 'rsync'
 nice -n5 rsync -auvx --delete --numeric-ids data.js index.html /var/www/lisakov.com/projects/open-source-comments/
 nice -n5 rsync -auvx --delete --numeric-ids images/ /var/www/lisakov.com/projects/open-source-comments/images/
 nice -n5 rsync -auvx --delete --numeric-ids css/ /var/www/lisakov.com/projects/open-source-comments/css/
 nice -n5 rsync -auvx --delete --numeric-ids js/ /var/www/lisakov.com/projects/open-source-comments/js/
 
 # update github repo  https://github.com/pozitron57/open-source-comments
+echo 'git add *'
 nice -n5 git add *
+echo 'git commit -m 'automatic update''
 nice -n5 git commit -m 'automatic update'
+echo 'git push origin master'
 nice -n5 git push origin master
