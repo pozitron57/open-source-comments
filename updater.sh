5d2ce0628c770f6d4f4e9705f55bdafa5912e320
pozitron57 <lisakov57@gmail.com>
2020-05-06 14:56:05 +0300

    automatic update

--- updater.sh
+++ updater.sh
@@ -22,10 +22,14 @@ nice -n5 python3 plot-stars.py
 
 # deploy changes
 echo 'rsync'
-nice -n5 rsync -auvx --delete --numeric-ids data.js index.html stars-v-date.png slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/
-nice -n5 rsync -auvx --delete --numeric-ids images/                             slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/images/
-nice -n5 rsync -auvx --delete --numeric-ids css/                                slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/css/
-nice -n5 rsync -auvx --delete --numeric-ids js/                                 slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/js/
+#nice -n5 rsync -auvx --delete --numeric-ids data.js index.html stars-v-date.png slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/
+#nice -n5 rsync -auvx --delete --numeric-ids images/                             slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/images/
+#nice -n5 rsync -auvx --delete --numeric-ids css/                                slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/css/
+#nice -n5 rsync -auvx --delete --numeric-ids js/                                 slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/js/
+nice -n5 rsync -auvx --delete --numeric-ids data.js index.html stars-v-date.png /var/www/lisakov.com/projects/open-source-comments/
+nice -n5 rsync -auvx --delete --numeric-ids images/                             /var/www/lisakov.com/projects/open-source-comments/images/
+nice -n5 rsync -auvx --delete --numeric-ids css/                                /var/www/lisakov.com/projects/open-source-comments/css/
+nice -n5 rsync -auvx --delete --numeric-ids js/                                 /var/www/lisakov.com/projects/open-source-comments/js/
 
 # update github repo  https://github.com/pozitron57/open-source-comments
 echo 'git add *'
