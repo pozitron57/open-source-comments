dd87a3fd4f859a24e1297128493595da866159ac
pozitron57 <lisako57@gmail.com>
2020-07-04 19:55:08 +0300

    automatic update

--- updater.sh
+++ updater.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-#cd /home/slisakov/yadisk/sites/open-source-comments
-cd /home/slisakov/open-source-comments
+## Local
+cd /home/slisakov/yadisk/sites/open-source-comments
+## Remote
+#cd /home/slisakov/open-source-comments
 echo 'git pull'
 nice -n5 git pull
 
@@ -22,14 +24,17 @@ nice -n5 python3 plot-stars.py
 
 # deploy changes
 echo 'rsync'
-#nice -n5 rsync -auvx --delete --numeric-ids data.js index.html stars-v-date.png slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/
-#nice -n5 rsync -auvx --delete --numeric-ids images/                             slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/images/
-#nice -n5 rsync -auvx --delete --numeric-ids css/                                slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/css/
-#nice -n5 rsync -auvx --delete --numeric-ids js/                                 slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/js/
-nice -n5 rsync -auvx --delete --numeric-ids data.js index.html stars-v-date.png /var/www/lisakov.com/projects/open-source-comments/
-nice -n5 rsync -auvx --delete --numeric-ids images/                             /var/www/lisakov.com/projects/open-source-comments/images/
-nice -n5 rsync -auvx --delete --numeric-ids css/                                /var/www/lisakov.com/projects/open-source-comments/css/
-nice -n5 rsync -auvx --delete --numeric-ids js/                                 /var/www/lisakov.com/projects/open-source-comments/js/
+## Local
+nice -n5 rsync -auvx --delete --numeric-ids data.js index.html stars-v-date.png slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/
+nice -n5 rsync -auvx --delete --numeric-ids images/                             slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/images/
+nice -n5 rsync -auvx --delete --numeric-ids css/                                slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/css/
+nice -n5 rsync -auvx --delete --numeric-ids js/                                 slisakov@lisakov.com:/var/www/lisakov.com/projects/open-source-comments/js/
+
+## Remote
+#nice -n5 rsync -auvx --delete --numeric-ids data.js index.html stars-v-date.png /var/www/lisakov.com/projects/open-source-comments/
+#nice -n5 rsync -auvx --delete --numeric-ids images/                             /var/www/lisakov.com/projects/open-source-comments/images/
+#nice -n5 rsync -auvx --delete --numeric-ids css/                                /var/www/lisakov.com/projects/open-source-comments/css/
+#nice -n5 rsync -auvx --delete --numeric-ids js/                                 /var/www/lisakov.com/projects/open-source-comments/js/
 
 # update github repo  https://github.com/pozitron57/open-source-comments
 echo 'git add *'
